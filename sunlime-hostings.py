694c57e25fd61a61ef02824a47d1cb0130409a2b
criscom <criscom27@gmail.com>
2017-12-12 13:36:10 +0100

    Set name for client

--- sunlime-hostings.py
+++ sunlime-hostings.py
@@ -1,44 +1,61 @@
 #! python 3
 # sunlime-hostings.py - create hosting contract change documents
 
-hostingOpening = 0
+hostingSalutation = 0
+hostingContinue = 0
 hosting = 0 # string to identify the hosting package; for EDIS it would be
 domain = 0 #
 date_1 = 0 # date for latest notification
 date_2 = 0 # date when server will be deleted
 choose = 'Choose how you want to address the client.'
-ms = 'Enter 1 for \"Frau\".'
-mr = 'Enter 2 for \"Herr\".'
+enterMs = 'Enter 1 for \"Frau\".'
+enterMr = 'Enter 2 for \"Herr\".'
+hostingSalutationMs = 'Sehr geehrte Frau '
+hostingSalutationMr = 'Sehr geehrter Herr '
 
-def hostingOpeningSet(hostingOpening):
-  print('{} \n {} \n {}'.format(choose,ms,mr))
-  hostingOpening = eval(input())
-  if hostingOpening in range(1, 3):
-    if hostingOpening == 1:
-      print('You have entered **{}** and chosen \"Frau\" as the opening greeting for your letter.'.format(hostingOpening))
-      return hostingOpening
+#################
+# Choose "Anrede"
+#################
 
-    elif hostingOpening == 2:
-      print('You have entered **{}** and chosen \"Herr\" as the opening greeting for your letter.'.format(hostingOpening))
-      return hostingOpening
+def hostingSalutationSet(hostingSalutation):
+  print('{} \n {} \n {}'.format(choose,enterMs,enterMr))
+  hostingSalutation = eval(input('>>> '))
+  if hostingSalutation in range(1, 3):
+    if hostingSalutation == 1:
+      print('You have entered **{}** and chosen \"Frau\" as the salutation for your letter.'.format(hostingSalutation))
+      print('Enter the surname of the client:')
+      hostingSalutationName = input('>>> ')
+      hostingSalutation = hostingSalutationMs + hostingSalutationName
+      print('Your salutation is: **{}**'.format(hostingSalutation))
+      return hostingSalutation
+
+    elif hostingSalutation == 2:
+      print('You have entered **{}** and chosen \"Herr\" as the salutation for your letter.'.format(hostingSalutation))
+      return hostingSalutation
 
   else:
     print('You haven\'t entered a correct number!','Let\'s start all over again!')
     print('=================================================================')
-    hostingOpeningSet(hostingOpening)
+    hostingSalutationSet(hostingSalutation)
 
 
+##################
+# Continue or quit
+##################
+
+def hostingContinueSet(hostingContinue):
+  quit = input('Quit program? \'q\' for quit, \'c\' for continue : ')
+  if quit == 'q':
+    print('Thank you for using my script! Have a good day!')
+    exit('Copyright Sunlime Web Innovations GmbH, 2018')
+  else:
+    print('Let\'s continue.')
 
 ##################
 # Start of script
 ##################
 
-hostingOpeningStatus = hostingOpeningSet(hostingOpening)
-  
-  # quit = input('Quit program? \'q\' for quit, \'c\' for contiune : ')
-  # if quit == 'q':
-  #   print('Thank you for using my script! Have a good day!')
-  #   print ('Copyright Sunlime Web Innovations GmbH, 2018')
-  #   break
-  # else:
-  #   print('Let\'s continue.')
+while True:
+  hostingSalutationStatus = hostingSalutationSet(hostingSalutation)
+  hostingContinueStatus = hostingContinueSet(hostingContinue)
+
